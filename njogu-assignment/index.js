339524a23a088bcf8f4ee2c7d7839a0fc71df543
munenebrian <njerubrianmunene@gmail.com>
2022-07-05 14:19:37 +0300

    reg-submission

---
+++ njogu-assignment/index.js
@@ -0,0 +1,12 @@
+function allnumeric(inputtxt) {
+    const numbers = /^[0-9]+$/;
+    if (inputtxt.value.match(numbers)) {
+        alert('Your Registration number has accepted....');
+        document.form1.text1.focus();
+        return true;
+    } else {
+        alert('Please input numeric characters only');
+        document.form1.text1.focus();
+        return false;
+    }
+}
